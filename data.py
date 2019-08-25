16446dc9568e5097b61416e98c2737051a70ebac
Mainul-Fahim <36801303+Mainul-Fahim@users.noreply.github.com>
2019-08-25 22:56:01 +0600

    Data
    product info is provided

---
+++ data.py
@@ -0,0 +1,44 @@
+# This are test data
+
+
+def products_info():
+    product = [
+        {'categoryID': 1,
+         'productID': 1,
+         'name': "Product1",
+         'price': 250},
+        {'categoryID': 1,
+         'productID': 2,
+         'name': "Product2",
+         'price': 250},
+        {'categoryID': 2,
+         'productID': 1,
+         'name': "Product1",
+         'price': 250},
+        {'categoryID': 2,
+         'productID': 2,
+         'name': "Product2",
+         'price': 250},
+        {'categoryID': 2,
+         'productID': 3,
+         'name': "Product3",
+         'price': 250},
+        {'categoryID': 2,
+         'productID': 4,
+         'name': "Product4",
+         'price': 250},
+        {'categoryID': 3,
+         'productID': 1,
+         'name': "Product1",
+         'price': 250}
+    ]
+    return product
+
+
+def category_info():
+    category = [
+        {'id': 1, 'name': "Category 1"},
+        {'id': 2, 'name': "Category 2"},
+        {'id': 3, 'name': "Category 3"},
+    ]
+    return category
