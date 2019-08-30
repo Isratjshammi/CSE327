2b3ee5f946a88fbdb87a9735c004133ffc503140
Isratjshammi <47280404+Isratjshammi@users.noreply.github.com>
2019-08-30 09:26:06 +0600

    Syncronizing all the files

--- main code/orange/venv/Scripts/easy_install-script.py
+++ main code/orange/venv/Scripts/easy_install-script.py
@@ -1,4 +1,4 @@
-#!E:\327github\CSE327\orange\venv\Scripts\python.exe
+#!"E:\327github\CSE327\main code\orange\venv\Scripts\python.exe"
 # EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==39.1.0','console_scripts','easy_install'
 __requires__ = 'setuptools==39.1.0'
 import re
--- main code/orange/venv/Scripts/pip3.7-script.py
+++ main code/orange/venv/Scripts/pip3.7-script.py
@@ -1,4 +1,4 @@
-#!E:\327github\CSE327\orange\venv\Scripts\python.exe
+#!"E:\327github\CSE327\main code\orange\venv\Scripts\python.exe"
 # EASY-INSTALL-ENTRY-SCRIPT: 'pip==10.0.1','console_scripts','pip3.7'
 __requires__ = 'pip==10.0.1'
 import re
